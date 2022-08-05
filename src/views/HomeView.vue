9dc521fef090f9b1750a0eddae9beaebdb96156c
Andy Furniss <afurniss4@googlemail.com>
2022-08-06 00:02:32 +0100

    Add borders and titles to distinguish between local and web video sources

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,13 @@
 
 <template>
   <main>
-    <video playsinline autoplay muted loop>
+    <h2>Local asset (../assets/big-buck-bunny_trailer.webm)</h2>
+    <video playsinline autoplay muted loop style="border:1px solid black;">
       <source src="../assets/big-buck-bunny_trailer.webm" width="311" height="600" type="video/webm" />
       Your browser does not support the video tag.
     </video>
-    <video playsinline autoplay muted loop>
+    <h2>Web reference (https://dl8.webmfiles.org/big-buck-bunny_trailer.webm)</h2>
+    <video playsinline autoplay muted loop style="border:1px solid black;">
       <source src="https://dl8.webmfiles.org/big-buck-bunny_trailer.webm" width="400" type="video/webm" />
       Your browser does not support the video tag.
     </video>
