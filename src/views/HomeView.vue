f758771d86964aa61689d9553c72f0192e3c96fd
Andy Furniss <afurniss4@googlemail.com>
2022-08-05 23:55:15 +0100

    Create new vue app and add webm videos

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+//import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <video playsinline autoplay muted loop>
+      <source src="../assets/big-buck-bunny_trailer.webm" width="311" height="600" type="video/webm" />
+      Your browser does not support the video tag.
+    </video>
+    <video playsinline autoplay muted loop>
+      <source src="https://dl8.webmfiles.org/big-buck-bunny_trailer.webm" width="400" type="video/webm" />
+      Your browser does not support the video tag.
+    </video>
+  </main>
+</template>
